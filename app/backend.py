0dbe6e68fabb829c719d702690847fee1a1a3a9c
Khachatryan-nn <tigrankh02@gmail.com>
2024-05-02 03:16:49 +0400

    Test

--- app/backend.py
+++ app/backend.py
@@ -11,13 +11,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def generate_message(link_length):
     return f"Hello, the length of the link is {link_length}."
 
-@app.get("/", response_class=HTMLResponse)
-async def read_item():
-    with open("/var/www/html/app/index.html", "r") as file:
-        html_content = file.read()
-    initial_message = generate_message(0)  # Initial message with link length 0
-    html_content = html_content.replace('<!--INITIAL_MESSAGE-->', initial_message)
-    return HTMLResponse(content=html_content)
+#@app.get("/", response_class=HTMLResponse)
+#async def read_item():
+#    with open("/var/www/html/app/index.html", "r") as file:
+#        html_content = file.read()
+#    initial_message = generate_message(0)  # Initial message with link length 0
+#    html_content = html_content.replace('<!--INITIAL_MESSAGE-->', initial_message)
+#    return HTMLResponse(content=html_content)
 
 @app.post("/")
 async def generate_response(link: str = Form(...)):
