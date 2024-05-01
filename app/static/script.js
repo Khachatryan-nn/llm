c7ce612c85051f13a13eb7f8a4e1f439b636ab4a
Khachatryan-nn <tigrankh02@gmail.com>
2024-05-02 03:42:45 +0400

    Back to Normal

--- app/static/script.js
+++ app/static/script.js
@@ -1,41 +1,41 @@
 async function displayInitialMessage() {
+	const response = await fetch("/");
     try {
-        const response = await fetch("/");
         const data = await response.json();
         document.getElementById("message").textContent = data.message;
         document.getElementById("message").style.display = "block"; // Show the message element
     } catch (error) {
-        console.error('Error parsing JSON from response:', error);
-        const responseText = await response.clone().text(); // Get the raw text of the response body
+		const responseText = await response.clone().text(); // Get the raw text of the response body
         console.log('Received the following instead of valid JSON:', responseText);
+        console.error('Error parsing JSON from response:', error);
     }
 }
 
 document.getElementById("linkForm").addEventListener("submit", async function(event) {
     event.preventDefault();
     var link = document.getElementById("linkInput").value;
-	console.log(link)
+	console.log(link);
     
+	const response = await fetch("/", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json", // Set content type to JSON
+		},
+		body: JSON.stringify({
+			link: link,
+		}),
+	});
+	console.log(response);
     try {
-        const response = await fetch("/", {
-            method: "POST",
-            headers: {
-				"Content-Type": "application/json", // Set content type to JSON
-			},
-			body: JSON.stringify({
-				link: link,
-			}),
-        });
-		console.log(response)
         const data = await response.json();
         
         var messageElement = document.getElementById("message");
         messageElement.innerHTML = "<p>" + data.message + "</p>";
         messageElement.style.display = "block"; // Show the message element
     } catch (error) {
-        console.error('Error parsing JSON from response:', error);
         const responseText = await response.clone().text(); // Get the raw text of the response body
         console.log('Received the following instead of valid JSON:', responseText);
+        console.error('Error parsing JSON from response:', error);
     }
 });
 
