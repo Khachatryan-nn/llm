65cde0734c83152fc127c531936014a28a5ac274
Khachatryan-nn <tigrankh02@gmail.com>
2024-05-02 03:04:08 +0400

    Little fix

--- app/backend.py
+++ app/backend.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
