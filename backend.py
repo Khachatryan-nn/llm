3f6bc71d80e1ca98481b9219290eb1e0a60666a7
Khachatryan-nn <tigrankh02@gmail.com>
2024-05-01 17:22:18 +0400

    Backend.py HTMLResponse function incorrect call fixed

--- backend.py
+++ backend.py
@@ -9,4 +9,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item():
-    return FileResponse("/var/www/html/index.html")
+    with open("/var/www/html/index.html", "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
