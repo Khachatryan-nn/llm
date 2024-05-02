eaf2e25ec4fd478904866eb9b1d3242383d4c613
Khachatryan-nn <tigrankh02@gmail.com>
2024-05-02 06:07:42 +0400

    Changes to HTML and JS

--- app/static/script.js
+++ app/static/script.js
@@ -1,36 +1,29 @@
 async function handleSubmit(event) {
-    event.preventDefault();
-    const link = document.getElementById("linkInput").value;
-    console.log('Link:', link);
+	event.preventDefault();
+	const link = document.getElementById("linkInput").value;
+	console.log('Link:', link);
   
-    try {
-        const formData = new URLSearchParams({ link });
-        const response = await fetch("/", {
-            method: "POST",
-            body: formData,
-        });
+	try {
+	  const formData = new URLSearchParams({ link });
+	  const response = await fetch("/", {
+		method: "POST",
+		body: formData,
+	  });
   
-        if (!response.ok) {
-            throw new Error('Failed to submit link');
-        }
+	  if (!response.ok) {
+		throw new Error('Failed to submit link');
+	  }
   
-        const responseData = await response.json();  // Parse response as JSON
-        const message = responseData.message;  // Extract message from response
-        console.log('Response message:', message);
-        
-        const messageElement = document.getElementById("message");
-        messageElement.innerHTML = message;  // Update HTML content with message
-        messageElement.style.display = "block";
-    } catch (error) {
-        console.error('Error submitting link:', error);
-    }
-}
+	  const responseData = await response.json();
+	  const message = responseData.message;
+	  console.log('Response message:', message);
   
-
-//function displayMessage(messageData) {
-//    const messageElement = document.getElementById("message");
-//    messageElement.textContent = messageData.message.replace(/<br>/g, "\n");
-//    messageElement.style.display = "block";
-//}
+	  const messageElement = document.getElementById("message");
+	  messageElement.innerHTML = message; // Update HTML content with message
+	  messageElement.style.display = "block";
+	} catch (error) {
+	  console.error('Error submitting link:', error);
+	}
+  }
 
 document.getElementById("linkForm").addEventListener("submit", handleSubmit);
